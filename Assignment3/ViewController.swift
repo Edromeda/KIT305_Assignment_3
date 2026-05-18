f59b953d304a80f1f3f4f5fd27c92fd10a251051
Chris Edrom <Chrissarcovez@gmail.com>
2026-05-18 19:05:21 +1000

    Firebase initialization

---
+++ Assignment3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment3
+//
+//  Created by Chris Edrom Luchavez on 18/5/2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
